ec76d6a95d6b382529e618389ef94e5a8f94d478
LPjavier <42053940+lugof@users.noreply.github.com>
2018-10-13 05:47:15 -0500

    fourth delivery

--- public/client.js
+++ public/client.js
@@ -5,7 +5,7 @@
 $(function(){
   var socket = io.connect();
   socket.on('stream', function(tweet) {
-    
+    console.log("agregando a arreglo  n veces");
 
   // $('body').append('<div class="tweet">' + tweet + '</div>')
    arr2.push(tweet);
